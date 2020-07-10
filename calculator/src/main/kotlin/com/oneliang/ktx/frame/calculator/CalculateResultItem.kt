c279708aae3e9145c34b1b4cde6c300f68af7763
oneliang <582199098@qq.com>
2020-07-10 19:12:41 +0800

    optimize default value for CalculateResultItem.kt

--- calculator/src/main/kotlin/com/oneliang/ktx/frame/calculator/CalculateResultItem.kt
+++ calculator/src/main/kotlin/com/oneliang/ktx/frame/calculator/CalculateResultItem.kt
@@ -5,5 +5,5 @@ import com.oneliang.ktx.Constants
 class CalculateResultItem(val name: String = Constants.String.BLANK,
                           val code: String = Constants.String.BLANK,
                           val inputJson: String = Constants.String.BLANK,
-                          var value: String = Constants.String.ZERO,
+                          var value: String = Constants.String.BLANK,
                           var codeType: String = Constants.String.BLANK)
