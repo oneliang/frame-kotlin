e33e47fbf8e7af61c793ffaceb35adc3b9f9e7fa
oneliang <582199098@qq.com>
2020-08-30 09:54:01 +0800

    optimize Ssh.kt

---
+++ ssh/src/main/kotlin/com/oneliang/ktx/frame/ssh/Ssh.kt
@@ -0,0 +1,67 @@
+package com.oneliang.ktx.frame.ssh
+
+import com.jcraft.jsch.*
+import com.oneliang.ktx.Constants
+import com.oneliang.ktx.util.common.perform
+import com.oneliang.ktx.util.logging.LoggerManager
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+
+object Ssh {
+    private val logger = LoggerManager.getLogger(Ssh::class)
+
+    object Configuration {
+        const val USERAUTH_GSSAPI_WITH_MIC = "userauth.gssapi-with-mic"
+        const val STRICT_HOST_KEY_CHECKING = "StrictHostKeyChecking"
+    }
+
+    fun connect(host: String, port: Int = 22, user: String, password: String, configurationMap: Map<String, String> = emptyMap(), connectTimeout: Int = 0, afterSessionConnect: (session: Session) -> Unit): Session {
+        val jSch = JSch()
+        val session = jSch.getSession(user, host, port)
+        session.setPassword(password)
+        configurationMap.forEach { (key, value) ->
+            session.setConfig(key, value);
+        }
+        session.connect(connectTimeout)
+        afterSessionConnect(session)
+        return session
+    }
+
+    fun shell(session: Session) {
+        val channelShell = session.openChannel("shell")
+        channelShell as ChannelShell
+        channelShell.inputStream = System.`in`
+        channelShell.outputStream = System.out
+        channelShell.connect()
+//        channelShell.disconnect()
+    }
+
+    fun exec(session: Session, command: String, afterChannelConnect: (channelExec: ChannelExec) -> Unit = {}) {
+        val channelExec = session.openChannel("exec")
+        logger.info("exec command:[%s]", command)
+        (channelExec as ChannelExec).setCommand(command)
+        channelExec.setInputStream(null)
+        channelExec.setErrStream(System.err)
+        channelExec.connect()
+        afterChannelConnect(channelExec)
+        channelExec.disconnect()
+    }
+
+    fun sftp(session: Session, afterChannelConnect: (channelSftp: ChannelSftp) -> Unit = {}) {
+        val channelSftp = session.openChannel("sftp")
+        channelSftp as ChannelSftp
+        channelSftp.connect()
+        perform({
+            afterChannelConnect(channelSftp)
+        }, failure = {
+            logger.error(decodeInputStream(channelSftp.inputStream), it)
+        })
+        channelSftp.disconnect()
+    }
+
+    fun decodeInputStream(inputStream: InputStream): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        inputStream.copyTo(byteArrayOutputStream)
+        return String(byteArrayOutputStream.toByteArray())
+    }
+}
--- ssh/src/test/kotlin/com/oneliang/ktx/frame/test/SshTestOne.kt
+++ ssh/src/test/kotlin/com/oneliang/ktx/frame/test/SshTestOne.kt
@@ -1,49 +1,17 @@
 package com.oneliang.ktx.frame.test
 
-import com.jcraft.jsch.Channel
-import java.io.File
 import javax.swing.JOptionPane
 
-
 fun main() {
-    val ssh = Ssh()
-    val host = "122.51.110.167"
-    val user = "root"
-    val password = JOptionPane.showInputDialog("Enter password")
-    val file = File("/D:/settings.zip")
-    val begin = System.currentTimeMillis()
-    ssh.connect(host, user = user, password = password, configMap = mapOf("userauth.gssapi-with-mic" to "no", "StrictHostKeyChecking" to "no"), afterSessionConnect = { session ->
-        val channel: Channel = session.openChannel("shell")
-        channel.inputStream = System.`in`
-        channel.outputStream = System.out
-        channel.connect()
-//        println("cost:" + (System.currentTimeMillis() - begin))
-//        Thread.sleep(5000)
-//        channel.disconnect()
-//        println("shell disconnect")
-//        val execChannel = session.openChannel("exec")
-//        (execChannel as ChannelExec).setCommand("ls")
-//        execChannel.setInputStream(null)
-//
-//        execChannel.setErrStream(System.err)
-//        execChannel.connect()
-//        val inputStream: InputStream = execChannel.getInputStream()
-//        val byteArrayOutputStream = ByteArrayOutputStream()
-//        inputStream.copyTo(byteArrayOutputStream)
-//        println(String(byteArrayOutputStream.toByteArray()))
-//        execChannel.disconnect()
-//        val directory = "/home/test"
-//        if (channel is ChannelSftp) {
-//            try { //执行列表展示ls 命令
-//                channel.ls(directory) //执行盘符切换cd 命令
-//                channel.cd(directory)
-//                val bufferedInputStream = BufferedInputStream(FileInputStream(file))
-//                channel.put(bufferedInputStream, file.name)
-//            } catch (t: Throwable) {
-//                t.printStackTrace()
-//                //mkdir when ls/cd exception
-//                channel.mkdir(directory)
-//            }
-//        }
-    })
+    val configuration = TomcatAutoUpdater.Configuration().apply {
+        this.host = "122.51.110.167"
+        this.port = 22
+        this.user = "root"
+        this.password = JOptionPane.showInputDialog("Enter password")
+        this.tomcatDirectory = "/home/wwwroot/apache-tomcat-backend"
+        this.localWarFullFilename = "/D:/settings.zip"
+        this.remoteWarName = "a.zip"
+    }
+    val tomcatAutoUpdater = TomcatAutoUpdater()
+    tomcatAutoUpdater.update(configuration)
 }
