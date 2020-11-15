45fb698e9daca3dc0c92a911f0987b0e634ca23a
oneliang <582199098@qq.com>
2020-11-15 21:21:28 +0800

    delete HttpCacheCollector.kt

--- collector/src/main/kotlin/com/oneliang/ktx/frame/collector/HttpCacheCollector.kt
+++
@@ -1,5 +0,0 @@
-package com.oneliang.ktx.frame.collector
-
-abstract class HttpCacheCollector<T> : Collector<HttpCacheCollector.From, T> {
-    class From(var httpUrl: String, var cacheDirectory: String)
-}
