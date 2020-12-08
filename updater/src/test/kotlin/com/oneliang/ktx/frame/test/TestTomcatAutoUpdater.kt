f4bf9f535c4938ec6c40f070b6cbf83c975978ea
oneliang <582199098@qq.com>
2020-12-08 15:20:30 +0800

    optimize TestTomcatAutoUpdater.kt

--- updater/src/test/kotlin/com/oneliang/ktx/frame/test/TestTomcatAutoUpdater.kt
+++ updater/src/test/kotlin/com/oneliang/ktx/frame/test/TestTomcatAutoUpdater.kt
@@ -1,8 +1,11 @@
 package com.oneliang.ktx.frame.test
 
+import com.oneliang.ktx.Constants
+import com.oneliang.ktx.frame.ssh.Ssh
 import com.oneliang.ktx.frame.updater.UpdaterExecutor
 import com.oneliang.ktx.frame.updater.tomcat.TomcatAutoUpdater
 import com.oneliang.ktx.util.common.perform
+import com.oneliang.ktx.util.common.readContentIgnoreLine
 import com.oneliang.ktx.util.file.readContentIgnoreLine
 import com.oneliang.ktx.util.json.jsonToObjectList
 import java.io.File
@@ -11,8 +14,8 @@ import javax.swing.JOptionPane
 
 fun main() {
     val configuration = TomcatAutoUpdater.Configuration().apply {
-        this.host = "122.51.110.167"
-        this.port = 22
+        this.host = "42.192.93.32"
+        this.port = 3222
         this.user = "root"
         this.password = JOptionPane.showInputDialog("Enter password:${this.host}")
         this.warArray = arrayOf(TomcatAutoUpdater.Configuration.War().apply {
@@ -21,7 +24,20 @@ fun main() {
             this.remoteWarName = "a.zip"
         })
     }
-    val configurationFile = File("")
+//    Ssh.connect(host = configuration.host,
+//            user = configuration.user,
+//            port = configuration.port,
+//            password = configuration.password,
+//            configurationMap = mapOf(Ssh.Configuration.USERAUTH_GSSAPI_WITH_MIC to "no", Ssh.Configuration.STRICT_HOST_KEY_CHECKING to "no"), afterSessionConnect = { session ->
+//        Ssh.exec(session, "java -version") {
+//            it.inputStream.readContentIgnoreLine { line ->
+//                println(line)
+//                true
+//            }
+//        }
+//    })
+//    return
+    val configurationFile = File(Constants.String.BLANK)
     val json = configurationFile.readContentIgnoreLine()
     val configurationList = json.jsonToObjectList(TomcatAutoUpdater.Configuration::class)
     val updaterExecutor = UpdaterExecutor()
@@ -34,7 +50,7 @@ fun main() {
             it.password
         })
         val tomcatAutoUpdater = TomcatAutoUpdater(it)
-        updaterExecutor.addTomcatAutoUpdater(tomcatAutoUpdater){
+        updaterExecutor.addTomcatAutoUpdater(tomcatAutoUpdater) {
             countDownLatch.countDown()
         }
     }
