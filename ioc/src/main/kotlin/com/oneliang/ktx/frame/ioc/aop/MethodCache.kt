93ef1b7dc94e416f3f0539410ae45dcc03e88cc6
oneliang <582199098@qq.com>
2021-01-28 09:39:09 +0800

    optimize MethodCache.kt

--- ioc/src/main/kotlin/com/oneliang/ktx/frame/ioc/aop/MethodCache.kt
+++ ioc/src/main/kotlin/com/oneliang/ktx/frame/ioc/aop/MethodCache.kt
@@ -4,11 +4,4 @@ import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class MethodCache(val cacheRefreshTime: Long = 0L) {
-    //val listFields: Array<ListField> = []) {
-
-    @MustBeDocumented
-    @Target(AnnotationTarget.ANNOTATION_CLASS)
-    @Retention(AnnotationRetention.RUNTIME)
-    annotation class ListField(val field: String, val kClass: KClass<*>)
-}
+annotation class MethodCache(val cacheRefreshTime: Long = 0L)
