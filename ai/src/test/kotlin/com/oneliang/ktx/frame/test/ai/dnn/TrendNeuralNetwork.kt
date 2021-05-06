fd811f59ec78df6037d126038e8becf45118e3e7
oneliang <582199098@qq.com>
2021-05-06 11:00:18 +0800

    backup code

--- ai/src/test/kotlin/com/oneliang/ktx/frame/test/ai/dnn/TrendNeuralNetwork.kt
+++ ai/src/test/kotlin/com/oneliang/ktx/frame/test/ai/dnn/TrendNeuralNetwork.kt
@@ -1,23 +1,26 @@
-package com.oneliang.ktx.frame.ai.dnn
+package com.oneliang.ktx.frame.test.ai.dnn
 
+import com.oneliang.ktx.frame.ai.dnn.NeuralNetwork
 import com.oneliang.ktx.frame.ai.dnn.layer.Layer
 import com.oneliang.ktx.frame.ai.dnn.layer.impl.FullyConnectedLayerImpl
 import com.oneliang.ktx.frame.ai.dnn.layer.impl.OutputLayerImpl
 import com.oneliang.ktx.util.logging.LoggerManager
 
-object DeepNeuralNetwork : NeuralNetwork {
-    private val logger = LoggerManager.getLogger((DeepNeuralNetwork::class))
+object TrendNeuralNetwork : NeuralNetwork {
+    private val logger = LoggerManager.getLogger((TrendNeuralNetwork::class))
 
     override fun getLayerList(): List<Layer<*, *>> {
         //input 2 hidden 3 and 2 output 1, input in batch
-        val inputLayer = FullyConnectedLayerImpl(3)
-        val hiddenLayer1 = FullyConnectedLayerImpl(2)
-        val hiddenLayer2 = FullyConnectedLayerImpl(1)
+        val inputLayer = FullyConnectedLayerImpl(30, true)
+        val hiddenLayer1 = FullyConnectedLayerImpl(20, true)
+        val hiddenLayer2 = FullyConnectedLayerImpl(10, true)
+        val hiddenLayer3 = FullyConnectedLayerImpl(1, true)
         val outputLayer = OutputLayerImpl()
         return listOf(
             inputLayer,
             hiddenLayer1,
             hiddenLayer2,
+            hiddenLayer3,
             outputLayer
         )
     }
