13da1d0cffaffa0c43fbde84b91b3fa5b4eaf4f0
oneliang <582199098@qq.com>
2021-05-20 15:29:52 +0800

    optimize some ai code

--- ai/src/main/kotlin/com/oneliang/ktx/frame/ai/activation/RectifiedLinearUnits.kt
+++ ai/src/main/kotlin/com/oneliang/ktx/frame/ai/activation/RectifiedLinearUnits.kt
@@ -1,5 +1,8 @@
 package com.oneliang.ktx.frame.ai.activation
 
+/**
+ * relu
+ */
 fun rectifiedLinearUnits(value: Float): Float {
     return if (value < 0.0f) 0.0f else value
 }
@@ -16,3 +19,21 @@ fun rectifiedLinearUnitsDerived(value: Double): Double {
     return if (value < 0.0) 0.0 else 1.0
 }
 
+/**
+ * leaky relu
+ */
+fun leakyRectifiedLinearUnits(value: Float, negativeValueSlope: Float = 0.1f): Float {
+    return if (value < 0.0f) negativeValueSlope * value else value
+}
+
+fun leakyRectifiedLinearUnits(value: Double, negativeValueSlope: Double = 0.1): Double {
+    return if (value < 0.0) negativeValueSlope * value else value
+}
+
+fun leakyRectifiedLinearUnitsDerived(value: Float, negativeValueSlope: Float = 0.1f): Float {
+    return if (value < 0.0f) negativeValueSlope else 1.0f
+}
+
+fun leakyRectifiedLinearUnitsDerived(value: Double, negativeValueSlope: Double = 0.1): Double {
+    return if (value < 0.0) negativeValueSlope else 1.0
+}
--- ai/src/main/kotlin/com/oneliang/ktx/frame/ai/activation/Softmax.kt
+++ ai/src/main/kotlin/com/oneliang/ktx/frame/ai/activation/Softmax.kt
@@ -6,39 +6,30 @@ import com.oneliang.ktx.util.concurrent.atomic.AtomicMap
 import com.oneliang.ktx.util.math.matrix.multiply
 import kotlin.math.exp
 
-private const val KEY_TOTAL_VALUE = "key_total_value"
 fun softmax(xArray: Array<Double>, weightArray: Array<Array<Double>>, negative: Boolean = false): Array<Double> {
-    val totalValueAtomicMap = AtomicMap<String, DoubleWrapper>()
-    val valueMatrix = xArray.multiply(weightArray, true, transform = {
+    var totalValue = 0.0
+    val valueMatrix = xArray.multiply(weightArray, false, transform = {
         val value = exp(it)
-        totalValueAtomicMap.operate(KEY_TOTAL_VALUE, create = { DoubleWrapper(value) },
-            update = { old ->
-                DoubleWrapper(old.value + value)
-//            totalValue += value
-            })
+        totalValue += value
         value
     })
     //update value matrix to probability
     for (index in valueMatrix.indices) {
-        valueMatrix[index] = valueMatrix[index] / totalValueAtomicMap[KEY_TOTAL_VALUE]!!.value * (if (negative) -1.0 else 1.0)
+        valueMatrix[index] = valueMatrix[index] / totalValue * (if (negative) -1.0 else 1.0)
     }
     return valueMatrix
 }
 
 fun softmax(xArray: Array<Float>, weightArray: Array<Array<Float>>, negative: Boolean = false): Array<Float> {
-    val totalValueAtomicMap = AtomicMap<String, FloatWrapper>()
+    var totalValue = 0.0f
     val valueMatrix = xArray.multiply(weightArray, true, transform = {
         val value = exp(it)
-        totalValueAtomicMap.operate(KEY_TOTAL_VALUE, create = { FloatWrapper(value) },
-            update = { old ->
-                FloatWrapper(old.value + value)
-//            totalValue += value
-            })
+        totalValue += value
         value
     })
     //update value matrix to probability
     for (index in valueMatrix.indices) {
-        valueMatrix[index] = valueMatrix[index] / totalValueAtomicMap[KEY_TOTAL_VALUE]!!.value * (if (negative) -1.0f else 1.0f)
+        valueMatrix[index] = valueMatrix[index] / totalValue * (if (negative) -1.0f else 1.0f)
     }
     return valueMatrix
 }
