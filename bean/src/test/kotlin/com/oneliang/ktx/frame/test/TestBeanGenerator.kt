7ccb395a159c5338a4c76e472bae6b5ab9b0f73e
oneliang <582199098@qq.com>
2022-04-16 21:25:00 +0800

    optimize bean module

--- bean/src/test/kotlin/com/oneliang/ktx/frame/test/TestBeanGenerator.kt
+++ bean/src/test/kotlin/com/oneliang/ktx/frame/test/TestBeanGenerator.kt
@@ -3,7 +3,7 @@ package com.oneliang.ktx.frame.test
 import com.oneliang.ktx.frame.bean.BeanGenerator
 
 fun main() {
-    val fullFilename = "D:/Dandelion/java/githubWorkspace/frame-kotlin/bean/src/main/resources/kotlin-bean.txt"
+    val fullFilename = "D:/Dandelion/java/githubWorkspace/frame-kotlin/bean/src/test/resources/kotlin-bean.txt"
     val beanTemplateFullFilename = "D:/Dandelion/java/githubWorkspace/frame-kotlin/bean/src/main/resources/kotlin-bean.tmpl"
     val baseOutputDirectory = "D:/Dandelion/java/githubWorkspace/frame-kotlin/bean/src/test/kotlin"
     BeanGenerator.generate(
