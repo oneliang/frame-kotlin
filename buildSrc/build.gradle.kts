bcf2ee6a69cde98724276becc7f12520e778e85d
oneliang <oneliang@microsteel.cn>
2023-03-29 20:53:16 +0800

    optimize script

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,12 @@ repositories {
     mavenCentral()
 }
 
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    }
+}
+
 dependencies {
     implementation(gradleApi())
 }
