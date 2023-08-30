73a072cb6cae51f34009cd61f651a7ef44cbfc1a
oneliang <oneliang@microsteel.cn>
2023-08-30 10:31:46 +0800

    optimize coroutine module

--- coroutine/src/main/kotlin/com/oneliang/ktx/frame/coroutine/Coroutine.kt
+++ coroutine/src/main/kotlin/com/oneliang/ktx/frame/coroutine/Coroutine.kt
@@ -7,7 +7,7 @@ import kotlin.coroutines.EmptyCoroutineContext
 
 class Coroutine(
     coroutineContext: CoroutineContext = EmptyCoroutineContext,
-    private val exceptionHandler: CoroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+    private val coroutineExceptionHandler: CoroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
         logger.error("Throws an exception with message: %s", throwable, throwable.message)
     }
 ) {
@@ -15,29 +15,49 @@ class Coroutine(
         private val logger = LoggerManager.getLogger(Coroutine::class)
     }
 
+    /**
+     * coroutine scope
+     */
     private val coroutineScope = object : CoroutineScope {
         override val coroutineContext: CoroutineContext
             get() = coroutineContext
     }
 
+    /**
+     * run blocking
+     * @param block
+     */
     fun runBlocking(block: suspend () -> Unit) {
-        runBlocking(this.coroutineScope.coroutineContext + this.exceptionHandler) {
+        runBlocking(this.coroutineScope.coroutineContext + this.coroutineExceptionHandler) {
             block()
         }
     }
 
+    /**
+     * launch
+     * @param block
+     * @return Job
+     */
     fun launch(block: suspend () -> Unit): Job {
-        return this.coroutineScope.launch(this.coroutineScope.coroutineContext + this.exceptionHandler) {
+        return this.coroutineScope.launch(this.coroutineScope.coroutineContext + this.coroutineExceptionHandler) {
             block()
         }
     }
 
+    /**
+     * sync
+     * @param block
+     */
     fun sync(block: suspend Coroutine.() -> Unit) {
         runBlocking {
             block(this)
         }
     }
 
+    /**
+     * async
+     * @param block
+     */
     fun async(block: suspend Coroutine.() -> Unit): Job {
         return launch {
             block(this)
