21750bd58e36a2f900c6740c0af851b55f8048a1
oneliang <oneliang@microsteel.cn>
2023-09-05 14:58:02 +0800

    optimize jdbc module

--- jdbc/src/main/kotlin/com/oneliang/ktx/frame/jdbc/JdbcExtensions.kt
+++ jdbc/src/main/kotlin/com/oneliang/ktx/frame/jdbc/JdbcExtensions.kt
@@ -31,49 +31,65 @@ fun String.toSqlNotEqual(value: String = Constants.Symbol.QUESTION_MARK): String
 }
 
 fun String.toSqlAndEqual(value: String = Constants.Symbol.QUESTION_MARK): String {
-    return " AND $this=$value"
+    return " ${Constants.Database.AND} $this=$value"
 }
 
 fun String.toSqlAndNotEqual(value: String = Constants.Symbol.QUESTION_MARK): String {
-    return " AND $this!=$value"
+    return " ${Constants.Database.AND} $this!=$value"
 }
 
 fun String.toSqlOrEqual(value: String = Constants.Symbol.QUESTION_MARK): String {
-    return " OR $this=$value"
+    return " ${Constants.Database.OR} $this=$value"
 }
 
 fun String.toSqlOrNotEqual(value: String = Constants.Symbol.QUESTION_MARK): String {
-    return " OR $this!=$value"
+    return " ${Constants.Database.OR} $this!=$value"
 }
 
 fun String.toSqlAndLike(value: String = Constants.Symbol.QUESTION_MARK): String {
-    return " AND $this LIKE $value"
+    return " ${Constants.Database.AND} $this ${Constants.Database.LIKE} $value"
 }
 
 fun String.toSqlOrLike(value: String = Constants.Symbol.QUESTION_MARK): String {
-    return " OR $this LIKE $value"
+    return " ${Constants.Database.OR} $this ${Constants.Database.LIKE} $value"
 }
 
-fun String.toSqlIn(subSql: String = Constants.String.BLANK): String {
-    return "$this IN ($subSql)"
+fun String.toSqlIn(subSql: String): String {
+    return "$this ${Constants.Database.IN} ($subSql)"
 }
 
-fun String.toSqlAndIn(subSql: String = Constants.String.BLANK): String {
-    return " AND $this IN ($subSql)"
+fun String.toSqlAndIn(subSql: String): String {
+    return " ${Constants.Database.AND} $this ${Constants.Database.IN} ($subSql)"
 }
 
-fun String.toSqlOrIn(subSql: String = Constants.String.BLANK): String {
-    return " OR $this IN ($subSql)"
+fun String.toSqlOrIn(subSql: String): String {
+    return " ${Constants.Database.OR} $this ${Constants.Database.IN} ($subSql)"
 }
 
 fun String.toSqlAs(columnAlias: String): String {
-    return "$this AS $columnAlias"
+    return "$this ${Constants.Database.AS} $columnAlias"
 }
 
 fun String.toSqlLeftJoin(subSql: String): String {
-    return "$this LEFT JOIN $subSql"
+    return "$this ${Constants.Database.LEFT_JOIN} $subSql"
 }
 
 fun String.toSqlOnEqual(value: String): String {
-    return " ON $this=$value"
+    return " ${Constants.Database.ON} $this=$value"
+}
+
+fun String.toSqlOrderByDesc(includeOrderBy: Boolean = true): String {
+    return if (includeOrderBy) {
+        "${Constants.Database.ORDER_BY} $this ${Constants.Database.DESC}"
+    } else {
+        "$this ${Constants.Database.DESC}"
+    }
+}
+
+fun String.toSqlOrderByAsc(includeOrderBy: Boolean = true): String {
+    return if (includeOrderBy) {
+        "${Constants.Database.ORDER_BY} $this ${Constants.Database.ASC}"
+    } else {
+        "$this ${Constants.Database.ASC}"
+    }
 }
