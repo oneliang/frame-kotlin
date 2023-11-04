e879d25f998adb3568d96dee68d56bbaea7cc9f1
oneliang <oneliang@microsteel.cn>
2023-11-04 16:24:34 +0800

    optimize task manager

--- task/src/main/kotlin/com/oneliang/ktx/frame/task/TaskManager.kt
+++ task/src/main/kotlin/com/oneliang/ktx/frame/task/TaskManager.kt
@@ -6,40 +6,114 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.asCoroutineDispatcher
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 
-class TaskManager(maxThreads: Int=1) {
+class TaskManager(maxThreads: Int = 1) {
     companion object {
         private val logger = LoggerManager.getLogger(TaskManager::class)
     }
 
     private val executorService = Executors.newFixedThreadPool(maxThreads)
     private val coroutine = Coroutine(executorService.asCoroutineDispatcher())
-    private val taskJobMap = ConcurrentHashMap<Int, Pair<Task, Job>>()
+    private val taskMap = ConcurrentHashMap<Int, Task>()
     private val cancelledTaskMap = ConcurrentHashMap<Int, Task>()
 
     /**
      * task definition
      */
-    abstract class Task {
-        @Volatile
-        internal var running = false
+    abstract class Task(val maxProcessors: Int = 1) {
 
-        internal suspend fun internalRun() {
-            this.running = true
-            run()
-            this.running = false
+        internal val processorJobMap = ConcurrentHashMap<Int, Pair<Processor, Job>>()
+        private val cancelledProcessorMap = ConcurrentHashMap<Int, Processor>()
+        private val runningProcessorCount = AtomicInteger(this.maxProcessors)
+
+        init {
+            if (this.maxProcessors <= 0) {
+                error("param[maxProcessors] must be large than 0")
+            }
+        }
+
+
+        /**
+         * internal run processor
+         * @param processor
+         * @param processorIndex
+         * @param taskFinishedCallback
+         */
+        internal suspend fun internalRunProcessor(
+            processor: Processor,
+            processorIndex: Int,
+            taskFinishedCallback: suspend (taskHashCode: Int) -> Unit
+        ) {
+            processor.running = true
+            try {
+                this.runProcessor(processorIndex)
+            } catch (e: Throwable) {
+                try {
+                    this.onCanceledProcessor(processorIndex)
+                } catch (e: Throwable) {
+                    logger.error("cancelCallback exception, processor:%s", e, processor)
+                } finally {
+                    this.cancelledProcessorMap[processorIndex] = processor
+                }
+                logger.error("cancelling coroutine job, processor:%s", e, processor)
+            } finally {
+                this.processorJobMap.remove(processorIndex)
+                processor.running = false
+                val runningCount = this.runningProcessorCount.decrementAndGet()
+                if (runningCount <= 0) {
+                    taskFinishedCallback(this.hashCode())
+                    try {
+                        onFinished()
+                    } catch (e: Throwable) {
+                        logger.error("finished execute exception, task:%s", e, this)
+                    }
+                }
+
+            }
         }
 
         /**
-         * run
+         * run processor
          */
-        abstract suspend fun run()
+        abstract suspend fun runProcessor(processorIndex: Int)
 
         /**
-         * cancel callback
+         * on canceled processor
          */
-        open suspend fun cancelCallback() {
+        open suspend fun onCanceledProcessor(processorIndex: Int) {
         }
+
+        /**
+         * on finished
+         */
+        open suspend fun onFinished() {
+        }
+
+        /**
+         * cancel all processor
+         */
+        fun cancelAllProcessor() {
+            logger.debug("cancel all task, task job map size:%s".format(this.processorJobMap.size))
+            this.processorJobMap.forEach { (key, processorJob) ->
+                val (processor, job) = processorJob
+                if (!processor.running) {
+                    this.cancelledProcessorMap[processor.hashCode()] = processor
+                }
+                job.cancel()
+            }
+            logger.debug("cancel all task, cancelled task map size:%s".format(this.cancelledProcessorMap.size))
+        }
+
+
+        /**
+         * processor definition
+         */
+        class Processor {
+            @Volatile
+            internal var running = false
+        }
+
     }
 
     /**
@@ -48,45 +122,33 @@ class TaskManager(maxThreads: Int=1) {
     fun startTask(
         task: Task,
         priority: Boolean = false
-    ): Job {
+    ) {
         if (priority) {
             this.cancelAllTask()
         }
         val hashCode = task.hashCode()
-        val job = this.coroutine.launch {
-            try {
-                task.internalRun()
-            } catch (e: Throwable) {
-                try {
-                    task.cancelCallback()
-                } catch (e: Throwable) {
-                    logger.error("cancelCallback exception, task hashCode:%s", e, hashCode)
-                } finally {
-                    this.cancelledTaskMap[hashCode] = task
-                }
-                logger.error("cancelling coroutine job, task hashCode:%s", e, hashCode)
-            } finally {
-                this.taskJobMap.remove(task.hashCode())
-                if (priority) {
-                    this.restoreAllCancelledTask()
+        for (processorIndex in 0 until task.maxProcessors) {
+            val processor = Task.Processor()
+            val job = this.coroutine.launch {
+                task.internalRunProcessor(processor, processorIndex) {
+                    this.taskMap.remove(hashCode)
                 }
             }
+            task.processorJobMap[processorIndex] = processor to job
         }
-        this.taskJobMap[hashCode] = task to job
-        return job
+        this.taskMap[hashCode] = task
+//        return job
     }
 
     /**
      * cancel all task
      */
     fun cancelAllTask() {
-        logger.debug("cancel all task, task job map size:%s".format(this.taskJobMap.size))
-        this.taskJobMap.forEach { (key, taskJob) ->
-            val (task, job) = taskJob
-            if (!task.running) {
-                this.cancelledTaskMap[task.hashCode()] = task
-            }
-            job.cancel()
+        logger.debug("cancel all task, task job map size:%s".format(this.taskMap.size))
+        this.taskMap.forEach { (hashCode, task) ->
+            task.cancelAllProcessor()
+            this.taskMap.remove(hashCode)
+            this.cancelledTaskMap[hashCode] = task
         }
         logger.debug("cancel all task, cancelled task map size:%s".format(this.cancelledTaskMap.size))
     }
@@ -96,8 +158,14 @@ class TaskManager(maxThreads: Int=1) {
      */
     fun restoreAllCancelledTask() {
         logger.debug("restore all cancelled task, cancelled task map size:%s".format(this.cancelledTaskMap.size))
-        this.cancelledTaskMap.forEach { (_, task) ->
+        this.cancelledTaskMap.forEach { (hashCode, task) ->
             this.startTask(task)
+            this.cancelledTaskMap.remove(hashCode)
         }
+    }
+
+    fun showTaskSize() {
+        println("task size:%s".format(this.taskMap.size))
+        println("cancel task size:%s".format(this.cancelledTaskMap.size))
     }
 }
--- task/src/test/kotlin/com/oneliang/ktx/frame/test/task/TestTaskManager.kt
+++ task/src/test/kotlin/com/oneliang/ktx/frame/test/task/TestTaskManager.kt
@@ -2,44 +2,65 @@ package com.oneliang.ktx.frame.test.task
 
 import com.oneliang.ktx.frame.task.TaskManager
 import kotlinx.coroutines.delay
+import java.util.*
 
 fun main() {
     val taskManager = TaskManager()
     val task1 = object : TaskManager.Task() {
-        override suspend fun run() {
+        override suspend fun runProcessor(processorIndex: Int) {
             println("task 1 run")
             delay(1000)
             println("task 1 run finish")
         }
 
-        override suspend fun cancelCallback() {
+        override suspend fun onCanceledProcessor(processorIndex: Int) {
             println("task 1 cancel")
+        }
+
+        override suspend fun onFinished() {
+            println("task 1 finished")
         }
     }
     val task2 = object : TaskManager.Task() {
-        override suspend fun run() {
+        override suspend fun runProcessor(processorIndex: Int) {
             println("task 2 run")
             delay(2000)
             println("task 2 run finish")
         }
 
-        override suspend fun cancelCallback() {
+        override suspend fun onCanceledProcessor(processorIndex: Int) {
             println("task 2 cancel")
+        }
+
+        override suspend fun onFinished() {
+            println("task 2 finished")
         }
     }
     val task3 = object : TaskManager.Task() {
-        override suspend fun run() {
+        override suspend fun runProcessor(processorIndex: Int) {
             println("task 3 run")
             delay(3000)
             println("task 3 run finish")
         }
 
-        override suspend fun cancelCallback() {
+        override suspend fun onCanceledProcessor(processorIndex: Int) {
             println("task 3 cancel")
+        }
+
+        override suspend fun onFinished() {
+            println("task 3 finished")
         }
     }
     taskManager.startTask(task1)
     taskManager.startTask(task2)
 //    taskManager.cancelAllTask()
     taskManager.startTask(task3, true)
+    taskManager.restoreAllCancelledTask()
+
+    val timer = Timer()
+    timer.schedule(object : TimerTask() {
+        override fun run() {
+            taskManager.showTaskSize()
+        }
+    }, 0, 1000)
 }
