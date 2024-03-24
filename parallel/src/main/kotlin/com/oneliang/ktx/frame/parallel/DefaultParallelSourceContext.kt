3491fa228f5aa8eb44261ca8daba59cc5294bd73
oneliang <oneliang@oneliang.com>
2024-03-24 12:38:03 +0800

    optimize some code

--- parallel/src/main/kotlin/com/oneliang/ktx/frame/parallel/DefaultParallelSourceContext.kt
+++ parallel/src/main/kotlin/com/oneliang/ktx/frame/parallel/DefaultParallelSourceContext.kt
@@ -1,16 +1,15 @@
 package com.oneliang.ktx.frame.parallel
 
 import com.oneliang.ktx.frame.coroutine.Coroutine
-import com.oneliang.ktx.util.logging.LoggerManager
 import com.oneliang.ktx.frame.parallel.cache.CacheData
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import com.oneliang.ktx.util.logging.LoggerManager
 
 internal class DefaultParallelSourceContext(
-        private val coroutine: Coroutine,
-        private val parallelSourceProcessor: ParallelSourceProcessor<Any?>,
-        private val parallelJobStepList: List<ParallelJobStep<Any?>>,
-        private val parallelJob: ParallelJob<Any?>) : ParallelSourceContext<Any?> {
+    private val coroutine: Coroutine,
+    private val parallelSourceProcessor: ParallelSourceProcessor<Any?>,
+    private val parallelJobStepList: List<ParallelJobStep<Any?>>,
+    private val parallelJob: ParallelJob<Any?>
+) : ParallelSourceContext<Any?> {
     companion object {
         private val logger = LoggerManager.getLogger(DefaultParallelSourceContext::class)
     }
@@ -25,9 +24,11 @@ internal class DefaultParallelSourceContext(
                     ParallelContextUtil.collectForParallelProcessor(this.coroutine, this.parallelJob, it, value, parallelContextAction)
                 }
             } else {
+                // for parallel source  processor
                 if ((parallelContextAction == ParallelContextAction.SAVEPOINT
-                                || parallelContextAction == ParallelContextAction.FINISHED)
-                        && this.parallelJob.parallelJobConfiguration.useCache) {
+                            || parallelContextAction == ParallelContextAction.FINISHED)
+                    && this.parallelJob.parallelJobConfiguration.useCache
+                ) {
                     val sourceKey = this.parallelSourceProcessor.cacheKey
                     val sourceData = this.parallelJob.getSourceData(sourceKey) ?: CacheData.Data()
                     this.parallelSourceProcessor.savepoint(sourceData)
